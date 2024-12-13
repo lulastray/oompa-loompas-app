4545554b6bec08a9956ef907154c8c191b8a0fb5
lulastray <luciaastray@gmail.com>
2024-12-13 06:08:34 +0100

    Download images

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import logoOompa from "../../assets/logo-umpa-loompa.png"
 
 function Layout({ children }: { children: React.ReactNode }) {
     const navigate = useNavigate()
@@ -9,7 +10,7 @@ function Layout({ children }: { children: React.ReactNode }) {
             <Box position="fixed" top="0" zIndex="9" width="1" bgcolor="#EDEDED">
                 <Box py={1} px={4} display="flex" alignItems="center" gap={3}>
                     <Box onClick={navigateToHome}>
-                        <img src="https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/logo-umpa-loompa.png" alt="Oompa Loompa Logo" style={{ height: '25px' }} />
+                        <img src={logoOompa} alt="Oompa Loompa Logo" style={{ height: '25px' }} />
                     </Box>
                     <Typography>Ommpa Loompa´s Crew</Typography>
                 </Box>
--- src/components/MainView/MainView.tsx
+++ src/components/MainView/MainView.tsx
@@ -1,5 +1,6 @@
-import { Box, Container, InputAdornment, TextField, Typography } from "@mui/material";
+import { Container, InputAdornment, Stack, TextField, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
+import searchImg from "../../assets/ic_search.png";
 import useOompaLoompas from "../../hooks/useOompaLoompas";
 import CardOompaLoompa from "./CardOompaLoompa";
 
@@ -21,44 +22,46 @@ function MainView() {
 
   return (
     <Container sx={{ height: "100vh" }}>
-      <Box pt={8}>
-       
-      <TextField
-        value={query}
-        onChange={(e)=>setQuery(e.target.value)}
-        placeholder="Search Oompa Loompas"
-        margin="normal"
-        slotProps={{
-          input: {
-        endAdornment:
-          <InputAdornment position="end">
-            <img src="https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/search.png" alt="search" />
-          </InputAdornment>}
-        }}
-      />
-    
-      <Typography variant="h4" component="h1">
-        Find your Oompa Loompa
-      </Typography>
-      <Typography variant="subtitle1" component="h1">
-        There are more than 100k
-      </Typography>
-      {isLoading && (
-        <Typography mt={2} variant="body1">
-          Loading...
+      <Stack pt={8} justifyContent="center" alignItems="center">
+        <Stack direction="row" justifySelf="start" width={1}>
+          <TextField
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search"
+            margin="normal"
+            slotProps={{
+              input: {
+                endAdornment:
+                  <InputAdornment position="end">
+                    <img src={searchImg} alt="search" style={{ height: '15px' }} />
+                  </InputAdornment>
+              }
+            }}
+          />
+        </Stack>
+        <Typography variant="h4" component="h1">
+          Find your Oompa Loompa
         </Typography>
-      )}
-      <Grid mt={4} container spacing={3}>
-        {oompaLoompas &&
-          oompaLoompas.map((oompaLoompa) =>{ 
-            return (
-            <Grid size={4} key={oompaLoompa.id}>
-              <CardOompaLoompa oompaLoompa={oompaLoompa} onHandleClick={onHandleClick}/>
-            </Grid>
-          )})}
-      </Grid>
-   
-      </Box>
+        <Typography variant="subtitle1" component="h1">
+          There are more than 100k
+        </Typography>
+        {isLoading && (
+          <Typography mt={2} variant="body1">
+            Loading...
+          </Typography>
+        )}
+        <Grid mt={4} container spacing={3}>
+          {oompaLoompas &&
+            oompaLoompas.map((oompaLoompa) => {
+              return (
+                <Grid size={4} key={oompaLoompa.id}>
+                  <CardOompaLoompa oompaLoompa={oompaLoompa} onHandleClick={onHandleClick} />
+                </Grid>
+              )
+            })}
+        </Grid>
+
+      </Stack>
     </Container>
   );
 }
